fa5c8d51ca23e3dc2df24e114710e51ee9fbeaf3
root <dux1996>
2018-05-27 08:54:07 +0530

    27-05-2018 by dux

--- app/src/main/java/in/dux/p013ab/SplashScreen.java
+++ app/src/main/java/in/dux/p013ab/SplashScreen.java
@@ -1,5 +1,7 @@
 package in.dux.p013ab;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    startActivity(new Intent(SplashScreen.this, Dashboard.class));
+                    finish();
+
+            }
+        },1000);
     }
 }
